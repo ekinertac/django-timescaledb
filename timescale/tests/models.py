cd4d9febdd8516164b82d24897af825236f92d79
ekinertac <ekinertac@gmail.com>
2026-03-30 15:43:33 +0300

    fix: add default time and ordering to test models

--- timescale/tests/models.py
+++ timescale/tests/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 from timescale.db.models.fields import TimescaleDateTimeField
 from timescale.db.models.managers import TimescaleManager
@@ -10,7 +11,7 @@ class Metric(models.Model):
     Test model using TimescaleDateTimeField directly (mirrors production usage pattern).
     Table name: timescale_tests_metric
     """
-    time = TimescaleDateTimeField(interval='1 day')
+    time = TimescaleDateTimeField(interval='1 day', default=timezone.now)
     temperature = models.FloatField(default=0.0)
     device = models.CharField(max_length=50, default='test-device')
 
@@ -19,6 +20,7 @@ class Metric(models.Model):
 
     class Meta:
         app_label = 'timescale_tests'
+        ordering = ['-time']
 
 
 class AnotherMetric(TimescaleModel):
@@ -26,7 +28,9 @@ class AnotherMetric(TimescaleModel):
     Test model using TimescaleModel abstract base class.
     Table name: timescale_tests_anothermetric
     """
+    time = TimescaleDateTimeField(interval='1 day', default=timezone.now)
     value = models.FloatField(default=0.0)
 
     class Meta:
         app_label = 'timescale_tests'
+        ordering = ['-time']
